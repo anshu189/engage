671fafe86441c394bf82434e7cc9ecacf3e11b63
anshusaini189381@gmail.com <anshusaini189381@gmail.com>
2024-05-07 06:54:06 +0530

    Form Validations and ReactHotToast

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Card,
   CardContent,
@@ -10,8 +12,37 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { ViewTransitions, Link } from 'next-view-transitions'
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
+
 
 const Login = () => {
+  
+  const router = useRouter();
+  const [logindisabled, setLogindisabled] = useState(true);
+  const loginotify = () => toast.success('Logged in SuccessFully!');
+  const [user, setuser] = useState({
+    password:"",
+    username:"",
+  })
+
+  const login = async ()=>{
+    console.log("Logged in!")
+    loginotify()
+  }
+
+  // Form Validations
+  useEffect(() => {
+    if(user.username.length > 2 && user.password.length > 5) {
+      setLogindisabled(false);
+    }
+    else{
+      setLogindisabled(true);
+    }
+  }, [user])
+  
+
   return (
     // <ViewTransitions>
       <div className='min-h-screen flex flex-col items-center justify-center gap-10 p-5 background foreground text-warning-foreground'>
@@ -25,18 +56,39 @@ const Login = () => {
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Username</Label>
-                <Input id="name" placeholder="elliot69" />
+                <Input id="name" placeholder="elliot69" value={user.username} onChange={(e)=>setuser({...user, username: e.target.value})}/>
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input type="email" placeholder="elliot69@fsociety.org" />
+                <Label htmlFor="email">Password</Label>
+                <Input type="password" placeholder="******" value={user.password} onChange={(e)=>setuser({...user, password: e.target.value})}/>
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline">Cancel</Button>
-          <Button>Login</Button>
+          <Button disabled={logindisabled} onClick={login}>Login</Button>
+          <Toaster toastOptions={{
+            success: {
+              duration:"150",
+              style: {
+                background: '#000',
+                color:"#fff",
+                // border: '1px solid #EA580C',  //themeOrange
+                border: '1px solid #1db853',  //successGreen
+              },
+              iconTheme: {
+                // primary: '#EA580C',  //themeOrange
+                primary: '#1db853',  //successGreen
+                secondary: '#fff',
+              },
+            },
+            error: {
+              style: {
+                background: 'red',
+              },
+            },
+          }}/>
         </CardFooter>
         <div className="flex items-center justify-center gap-2 pb-4">
         <span className='opacity-40'>Don't have an account?</span> <Link href="/signup" className='text-orange-500'>Signup</Link>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,6 @@
 'use client';
-import React from 'react'
+
+import React, { useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -15,10 +16,15 @@ import { Label } from "@/components/ui/label"
 
 import axios from 'axios';
 import { useState } from 'react';
+import { sign } from 'jsonwebtoken';
 import { useRouter } from 'next/navigation';
-import { ViewTransitions, Link } from 'next-view-transitions'
+import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'next-view-transitions'
 
 const Signup = () => {
+  const router = useRouter();
+  const signupnotify = () => toast.success('Welcome onboard, Normie.');
+  const [singupdisabled, setSingupdisabled] = useState(true);
   const [user, setuser] = useState({
     email:"",
     password:"",
@@ -26,9 +32,21 @@ const Signup = () => {
   })
 
   const signup = async ()=>{
-    console.log("Welcome Onboard, Newbie!")
+    console.log("Welcome Onboard, Normie!")
+    signupnotify();
   }
 
+  // Form Validations
+  useEffect(() => {
+    const isValidEmail = user.email.includes("@") && (user.email.endsWith(".com") || user.email.endsWith(".co") || user.email.endsWith(".org") || user.email.endsWith(".net") || user.email.endsWith(".in") || user.email.endsWith(".uk") || user.email.endsWith(".fr") || user.email.endsWith(".edu"));
+    if (isValidEmail && user.username.length > 2 && user.password.length > 5) {
+        setSingupdisabled(false);
+    }
+    else{
+      setSingupdisabled(true);
+    }
+  }, [user])
+  
   return (
     // <ViewTransitions>
       <div className='min-h-screen flex flex-col items-center justify-center gap-10 p-5 background foreground text-warning-foreground'>
@@ -42,18 +60,43 @@ const Signup = () => {
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Username</Label>
-                <Input id="name" placeholder="elliot69" />
+                <Input id="name" placeholder="elliot69" value={setuser.username} onChange={(e)=>setuser({...user, username: e.target.value})}/>
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
-                <Input type="email" placeholder="elliot69@fsociety.org" />
+                <Input type="email" placeholder="elliot69@fsociety.org" value={setuser.email} onChange={(e)=>setuser({...user, email: e.target.value})}/>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" placeholder="******" value={setuser.password} onChange={(e)=>setuser({...user, password: e.target.value})}/>
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline">Cancel</Button>
-          <Button>Signup</Button>
+          <Button disabled={singupdisabled} onClick={signup}>Signup</Button>
+          <Toaster toastOptions={{
+            success: {
+              duration:"150",
+              style: {
+                background: '#000',
+                color:"#fff",
+                border: '1px solid #EA580C',  //themeOrange
+                // border: '1px solid #1db853',  //successGreen
+              },
+              iconTheme: {
+                primary: '#EA580C',  //themeOrange
+                // primary: '#1db853',  //successGreen
+                secondary: '#fff',
+              },
+            },
+            error: {
+              style: {
+                background: 'red',
+              },
+            },
+          }}/>
         </CardFooter>
         <div className="flex items-center justify-center gap-2 pb-4">
           <span className='opacity-40'>Already have an account?</span> <Link href="/login" className='text-orange-500'>Login</Link>
